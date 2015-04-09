0397c782f41830287bba4603c5ecc952be3ca60a
eduardo-moraes <eduardo@heliopausedev.com>
2015-04-09 12:16:59 -0500

    first test

---
+++ WorldSimulation/src/Driver.java
@@ -0,0 +1,11 @@
+
+//import world.*;
+//import ui.*;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		//World theWorld = new World(10.0, 10.0);
+		//WorldView mWorldView = new WorldView(theWorld);
+	}
+}
